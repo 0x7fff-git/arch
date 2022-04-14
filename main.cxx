e48c95a5344c9d8cc657b84442535997e0cb2bfa
0x7fff-git <this.transform.identity@gmail.com>
2022-04-14 19:57:57 +0200

    added source

--- main.cxx
+++ main.cxx
@@ -1,87 +1,100 @@
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
-#include "image.hxx"
-#include "font.hxx"
 
-std::string exec(const char* cmd) {
-    std::array<char, 128> buffer;
-    std::string result;
-    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-    if ( !pipe )
-      return { };
-    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
-        result += buffer.data();
-    }
-    return result;
-}
+#include <filesystem>
+#include <iostream>
+#include <fstream>
+#include <string>
 
-std::int32_t main( ) noexcept {
-   sf::RenderWindow window{ { 700, 100 }, "run", 0 };
-   window.setFramerateLimit( 60 );
-   window.setVerticalSyncEnabled( 1 );
-   window.setPosition( { ( sf::VideoMode::getDesktopMode( ).width * 0.5f ) - 350,
-                         ( sf::VideoMode::getDesktopMode( ).height * 0.5f ) - 50 } );
-   sf::Font font{ };
-   font.loadFromMemory( font_bytes, font_byte_length );
-   sf::Text text{ "", font, 20 };
-   text.setPosition( { 148, 37 } );
-   std::string input_text{ };
-   while ( window.isOpen( ) ) {
-      if ( !window.hasFocus( ) )
-         window.close( );
-      sf::Event event{ };
-      while ( window.pollEvent( event ) ) {
-         switch ( event.type ) {
-         case sf::Event::TextEntered:
-            if ( !std::isprint( event.text.unicode ) )
-               break;
-            { sf::Vector2f position{ }; 
-               if ( !input_text.empty( ) )
-                  position = text.findCharacterPos( input_text.length( ) );
-               if ( position.x <= 590 )
-                  input_text += event.text.unicode;
-            }
-            break;
+#include "dep/font.hxx"
+#include "dep/back.hxx"
+
+std::int32_t main( ) {
+   const auto sf_path = std::string{ std::getenv( "HOME" ) } + "/.config/xorg/.run";
+   if ( !std::filesystem::exists( sf_path ) )
+      return 0;
+   
+   std::ifstream sf_conf{ sf_path };
+   if ( !sf_conf.is_open( ) || !sf_conf.good( ) )
+      return 0;
+   std::string sf_from_conf{ };
+   while ( std::getline( sf_conf, sf_from_conf ) )
+      if ( !sf_from_conf.empty( ) && std::filesystem::exists( sf_from_conf ) )
+         break;
+   
+   const sf::RenderWindow sf_win{ { 700, 100 }, "", 0 };
+   sf_win.setPosition( { 610, 490 } );
+   sf_win.setFramerateLimit( 90 );
+   
+   const sf::Font sf_font{ };
+   sf_font.loadFromMemory( mem_font, sizeof( mem_font ) );
+   
+   const sf::Texture sf_ava{ };
+   sf_ava.loadFromFile( sf_from_conf );
+   sf_ava.setSmooth( 1 );
+   sf_ava.generateMipmap( );
+   
+   const sf::Texture sf_bck{ };
+   sf_bck.loadFromMemory( mem_bck, sizeof( mem_bck ) );
+   sf_bck.setSmooth( 1 );
+   sf_bck.generateMipmap( );
+   
+   const sf::Sprite sf_bck_sprite{ sf_bck };
+   const sf::Sprite sf_ava_sprite{ sf_ava };
+   
+   sf_ava_sprite.setScale( 100.f / sf_ava.getSize( ).x, 100.f / sf_ava.getSize( ).y );
+   
+   const sf::Text sf_text{ };
+   sf_text.setFont( sf_font );
+   sf_text.setCharacterSize( 20 );
+   sf_text.setFillColor( { 225, 191, 245 } );
+   sf_text.setPosition( { 143 , 37 } );
+   
+   sf::Event sf_event{ };
+   std::string sf_input{ };
+   
+   while ( sf_win.isOpen( ) ) {
+      if ( sf_win.pollEvent( sf_event ) ) {
+         switch ( sf_event.type ) {
          case sf::Event::KeyPressed:
-            switch ( event.key.code ) {
-            case sf::Keyboard::Escape:
-               window.close( );
-               break;
-            case sf::Keyboard::BackSpace:
-               if ( !input_text.empty( ) )
-                  input_text.pop_back( );
-               break;
-            case sf::Keyboard::Return:
-               if ( input_text.length( ) > 0 )
-                  std::system( std::string{ input_text + " &" }.c_str( ) );
-               window.close( );
-               break;
+            if ( !sf_input.empty( ) ) {
+               if ( sf_event.key.code == sf::Keyboard::BackSpace )
+                  sf_input.pop_back( );
+                  
+               if ( sf_event.key.code == sf::Keyboard::Return ) {
+                  std::system( std::string( sf_input + " & > /dev/null" ).c_str( ) );
+                  sf_win.close( );
+               }
             }
+            
+            if ( sf_event.key.code == sf::Keyboard::Escape
+              || sf_event.key.code == sf::Keyboard::LAlt
+              || sf_event.key.code == sf::Keyboard::LSystem )
+               [[ fallthrough ]]
+               
+         case sf::Event::Closed:
+         case sf::Event::LostFocus:
+            sf_win.close( );
+            break;
+            
+         case sf::Event::TextEntered:
+            if ( std::isprint( sf_event.text.unicode ) && sf_text.findCharacterPos( -1 ).x <= 600 )
+               sf_input += static_cast< const std::uint8_t >( sf_event.text.unicode );
             break;
          }
       }
-      if ( sf::Keyboard::isKeyPressed( sf::Keyboard::LControl ) ) {
-         if ( sf::Keyboard::isKeyPressed( sf::Keyboard::BackSpace ) )
-            input_text = "";
-         else if ( sf::Keyboard::isKeyPressed( sf::Keyboard::V ) ) {
-            auto input = exec( "echo -n $( xclip -o )" );
-            if ( input.length( ) < 35 )
-               input_text = input;
-         }
-         else if ( sf::Keyboard::isKeyPressed( sf::Keyboard::C ) ) {
-            std::system( std::string{ "echo -n '" + input_text + "' | xclip -i " }.c_str( ) );
-         }
-      }
-      sf::Image image{ };
-      image.loadFromMemory( image_bytes, image_byte_length );
-      sf::Texture texture{ };
-      texture.loadFromImage( image );
-      sf::Sprite sprite{ };
-      sprite.setTexture( texture, 1 );
-      text.setString( input_text + "_" );
-      text.setFillColor( { 225, 191, 245 } );
-      window.clear( );
-      window.draw( sprite );
-      window.draw( text );
-      window.display( );
+      
+      if ( sf::Keyboard::isKeyPressed( sf::Keyboard::LControl )
+        && sf::Keyboard::isKeyPressed( sf::Keyboard::BackSpace ) )
+        sf_input = "";
+      
+      sf_text.setString( sf_input + "_" );
+      
+      sf_win.clear( );
+      sf_win.draw( sf_ava_sprite );
+      sf_win.draw( sf_bck_sprite );
+      sf_win.draw( sf_text );
+      sf_win.display( );
    }
 }
